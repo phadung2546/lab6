2ded333f4f5d22fc908abc6fb34605b830d91692
phadung2546 <psb2546@gmail.com>
2023-01-22 22:19:01 +0700

    lab5

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import LikeSad from "./components/LikeSad.vue";
+import { ref } from "vue";
+
+const stuid = ref("");
+const stuName = ref("");
+</script>
+
+<template>
+
+  <div v-for="n in 6" :key="n">{{  }}<LikeSad> </LikeSad></div>
+
+</template>
+
---
+++ src/components/LikeSad.vue
@@ -0,0 +1,52 @@
+Phadungkiat Sutayo
+<script setup>
+import { ref } from "vue";
+const like = ref(0);
+const sad = ref(0);
+
+function likeClick() {
+  like.value++;
+}
+function sadClick() {
+  sad.value++;
+}
+</script>
+
+<template>
+  <center>
+    <div class="col">
+      <div class="card shadow-sm">
+        <svg
+          class="bd-placeholder-img card-img-top"
+          width="450"
+          height="225"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          aria-label="Placeholder: Thumbnail"
+          preserveAspectRatio="xMidYMid slice"
+          focusable="false"
+        >
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#CCFFFF"></rect>
+          <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+        </svg>
+        <div class="card-body">
+          <p class="card-text">
+            This is a wider card with supporting text below as natural lead-in to
+            additional content. This content is a little bit longer.
+          </p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <button @click="likeClick()">ปุ่ม like{{ like }}</button>
+
+      <button @click="sadClick()">ปุ่ม sad {{ sad }}</button>
+    </div>
+  </center>
+</template>
+
+<style scoped></style>
